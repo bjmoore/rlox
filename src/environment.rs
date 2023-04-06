0ad3363d89bc36c4dede8b43adf1695908327aa1
Benjamin Moore <benjaminjm@gmail.com>
2023-04-05 21:08:05 -0700

    Add push/pop methods for scoped environments

--- src/environment.rs
+++ src/environment.rs
@@ -2,18 +2,37 @@ use std::collections::HashMap;
 
 use crate::value::LoxValue;
 pub struct Environment {
-    pub values: HashMap<String, LoxValue>,
+    values: HashMap<String, LoxValue>,
+    parent: Option<Box<Environment>>,
 }
 
 impl Environment {
     pub fn new() -> Self {
         Self {
             values: HashMap::new(),
+            parent: None,
         }
     }
 
-    pub fn get(&self, name: &str) -> &LoxValue {
-        self.values.get(name).unwrap_or(&LoxValue::Nil)
+    pub fn push_env(self) -> Self {
+        Self {
+            values: HashMap::new(),
+            parent: Some(Box::new(self)),
+        }
+    }
+
+    pub fn pop_env(self) -> Option<Box<Self>> {
+        self.parent
+    }
+
+    pub fn get(&self, name: &str) -> LoxValue {
+        match self.values.get(name) {
+            Some(value) => value.clone(),
+            None => match &self.parent {
+                Some(environment) => environment.get(name),
+                None => LoxValue::Nil,
+            },
+        }
     }
 
     pub fn put(&mut self, name: String, value: LoxValue) {
